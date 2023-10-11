348db8217c1b994c2727b2f97df0587300a8ba51
steven <stevenzhao0819@gmail.com>
2023-10-10 22:29:28 -0400

    Add Login input state

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,3 +1,32 @@
+import { Box, Button, TextField, Typography } from "@mui/material";
+import { ChangeEvent, FormEvent, useState } from "react";
+
 export default function Login() {
-    return <div>Login</div>
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+
+    const handleClick = (e: FormEvent) => {
+        e.preventDefault()
+    }
+
+    const handleEmailChange = (e: ChangeEvent<HTMLInputElement>) => {
+        setEmail(e.target.value)
+    }
+
+
+    return <Box display="flex" justifyContent="center" alignItems="center" flexDirection="column" height="80vh">
+        <Typography variant="h3">Welcome</Typography>
+        <Box component="form" width={400}>
+            <Box>
+                <TextField value={email} onChange={handleEmailChange} label="email" margin="normal" fullWidth required type="email" />
+            </Box>
+            <Box>
+                <TextField value={password} onChange={e => setPassword(e.target.value)} label="password" margin="normal" fullWidth required type="password" />
+            </Box>
+            <Box my={3}>
+                <Button type="submit" variant="contained" fullWidth onClick={handleClick}>Login</Button>
+            </Box>
+        </Box>
+    </Box>
 }
